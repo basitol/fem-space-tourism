d1051b15c5b65832c49470cef53251799cdd3e69
basitol <41623944+basitol@users.noreply.github.com>
2022-03-20 07:56:38 +0100

    Creating the navlinks

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,6 +11,31 @@ const Header = () => {
             <img src={logo} alt="Space Tourism" title="Space Tourism" />
           </Link>
         </div>
+
+        <nav className="navbar">
+          <ul>
+            <li>
+              <Link to="/">
+                <span>00</span>HOME
+              </Link>
+            </li>
+            <li>
+              <Link to="/destination">
+                <span>0</span>DESTINATION
+              </Link>
+            </li>
+            <li>
+              <Link to="/crew">
+                <span>02</span>CREW
+              </Link>
+            </li>
+            <li>
+              <Link to="/technology">
+                <span>03</span>TECHNOLOGY
+              </Link>
+            </li>
+          </ul>
+        </nav>
       </header>
     </>
   );
