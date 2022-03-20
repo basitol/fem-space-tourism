5e9c8a7cb7a6e176ff33aa0387938fb8f08b9e50
basitol <41623944+basitol@users.noreply.github.com>
2022-03-20 02:13:53 +0100

    Edited the home page

---
+++ src/components/Home.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Home = () => {
+  return (
+    <>
+      <section className="home">
+        <div className="overlay>">
+          <article>
+            <h1>
+              SO YOU WANT TO TRAVEL TO SPACE<span>SPACE</span>
+            </h1>
+            <p>
+              Let’s face it; if you want to go to space, you might as well
+              genuinely go to outer space and not hover kind of on the edge of
+              it. Well sit back, and relax because we’ll give you a truly out of
+              this world experience!
+            </p>
+          </article>
+          <article>
+            <Link to="./destination">Explore</Link>
+          </article>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Home;
