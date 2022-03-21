557f8975a2e6b3594a22d9c2eca907fa1dacfdb0
basitol <41623944+basitol@users.noreply.github.com>
2022-03-21 02:47:53 +0100

    Editing the navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,41 +1,47 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import logo from "../assets/shared/logo.svg";
+import { CgMenuLeft } from "react-icons/cg";
 
 const Header = () => {
   return (
     <>
-      <header className="header">
-        <div>
+      <header className="header flex flex-jc-sb w-full">
+        <div className="logo">
           <Link to="/">
             <img src={logo} alt="Space Tourism" title="Space Tourism" />
           </Link>
         </div>
 
-        <nav className="navbar">
+        <nav className="navbar open">
           <ul>
             <li>
               <Link to="/">
-                <span>00</span>HOME
+                <span className="num">00</span>HOME
               </Link>
             </li>
             <li>
               <Link to="/destination">
-                <span>0</span>DESTINATION
+                <span className="num">01</span>DESTINATION
               </Link>
             </li>
             <li>
               <Link to="/crew">
-                <span>02</span>CREW
+                <span className="num">02</span>CREW
               </Link>
             </li>
             <li>
               <Link to="/technology">
-                <span>03</span>TECHNOLOGY
+                <span className="num">03</span>TECHNOLOGY
               </Link>
             </li>
           </ul>
         </nav>
+        <div className="micon">
+          <button>
+            <CgMenuLeft className="text-4xl" />
+          </button>
+        </div>
       </header>
     </>
   );
