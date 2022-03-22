b43eba3913f00afab34f2cd6ec89d1a7847aad11
basitol <41623944+basitol@users.noreply.github.com>
2022-03-22 23:16:25 +0100

    Adding the basic state for destination page

--- src/pages/Destination.jsx
+++ src/pages/Destination.jsx
@@ -1,6 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
+import { destinations } from "../data.json";
 
 const Destination = () => {
+  const [planets] = useState(destinations);
+  const [value, setValue] = useState(0);
+
+  const { name, images, description, distance, travel } = planets[value];
+
   return (
     <div>
       <h1>Destination page</h1>
